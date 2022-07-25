a25e972347f8a3077b6c27c432e8f48dfa66f10b
Sally <peacheeks@gmail.com>
2022-07-25 23:04:11 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-import { useEffect, useState } from 'react';
-import './App.css';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    );
-    const json = await response.json(); 
-    setMovies(json.data.movies);
-    setLoading(false);
-    };
-
-  useEffect(() => {
-    getMovies()
-  }, [])
-  return (
-    <div>
-      {loading ? ( <h1>loading...</h1> 
-      ) : (
-      <div>{movies.map((movie) => (
-       <div key={movie.id}>
-        <img src={movie.medium_cover_image} />
-        <h2>{movie.title}</h2>
-        <p>{movie.summary}</p>
-        <ul>
-          <li>
-            {movie.genres.map(g => <li key={g}>{g}</li>)}
-          </li>
-        </ul>
-      </div>
-       ))}
-       </div>
-      )}
-      
-    </div>
-  );
+  return 
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Detail />} />
+      </Routes>
+    </Router>
 }
-
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,4 @@
+function Detail(){
+    retrun <h1>Detail</h1>
+}
+export default Detail;
